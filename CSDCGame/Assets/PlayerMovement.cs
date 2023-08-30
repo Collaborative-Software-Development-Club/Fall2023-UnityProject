844f2ed4a25e02e5843f8329c87f1998a2e1a191
holmes301 <quintonrholmes30@gmail.com>
2023-08-30 19:59:53 -0400

    Movement Script

--- CSDCGame/Assets/PlayerMovement.cs
+++ CSDCGame/Assets/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody playerRB;
+    public float speed;
+    private Vector3 _playerMovement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRB.velocity = _playerMovement * speed;
+    }
+    void FixedUpdate() {
+        // "Vertical" and "Horizontal"
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        _playerMovement = new Vector3(x, 0, z);
     }
 }
