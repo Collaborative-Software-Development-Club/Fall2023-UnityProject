8431f6001d64bf052a880511379193db8e2d74e6
holmes301 <quintonrholmes30@gmail.com>
2023-09-22 19:33:12 -0400

    Pause Functionality
    Completed pause functionality. Quit functionality will be complete once the start menu is complete.

---
+++ CSDCGame/Assets/PauseScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject gameUI;
+    public GameObject pauseUI;
+    public Button resumeButton;
+    public Button quitButton;
+    private bool _isPaused = false;
+
+    void Start() {
+        resumeButton.onClick.AddListener(ResumeGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            _isPaused = !_isPaused;
+            gameUI.SetActive(!_isPaused);
+            pauseUI.SetActive(_isPaused);
+            if (_isPaused) {
+                PauseGame();
+            }
+            else {
+                ResumeGame();
+            }
+        }
+    }
+    private void PauseGame() {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame() {
+        Time.timeScale = 1.0f;
+        _isPaused = false;
+        pauseUI.SetActive(_isPaused);
+        gameUI.SetActive(!_isPaused);
+    }
+    public void QuitGame() {
+        // TODO: QUIT GAME HERE
+    }
+}
