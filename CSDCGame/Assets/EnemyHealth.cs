1b56411245f1f03dfde4963abfe02ff282f0ee9b
Tristan Leedy <117492192+eldenleedy@users.noreply.github.com>
2023-11-01 20:42:25 -0400

    Enemy Health & Bullet Collision
    Enemy now starts with health, and takes damage from bullets. When health reaches 0, the enemy game object is destroyed.

---
+++ CSDCGame/Assets/EnemyHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    public float damageCooldown = 1f;
+    private float lastDamageTime;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        lastDamageTime = -damageCooldown;
+    }
+    public void TakeDamage(int damageAmount)
+    {
+        if (Time.time - lastDamageTime >= damageCooldown)
+        {
+            currentHealth -= damageAmount;
+            lastDamageTime = Time.time;
+            Debug.Log("Damaged enemy, " + currentHealth + " health left");
+            if (currentHealth <= 0)
+            {
+                Destroy(gameObject);
+                Debug.Log("Killed enemy");
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Projectile")
+        {
+            TakeDamage(1);
+        }
+    }
+}
